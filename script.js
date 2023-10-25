cec581ed061de0eb6e317fb61299082929f9d933
Carlos A. Herrera <zenndeuphoric@gmail.com>
2023-10-25 00:10:36 -0400

    made it so website waits before eventhandlers

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function addExpense(date, amount, description, paidBy) {
     const expense = { date, amount, description, paidBy };
     expenses.push(expense);
 }
-console.log("hello")
+console.log("hel5555lo")
 
 // Function to display expenses in the expense-list section
 function displayExpenses() {
@@ -26,31 +26,31 @@ function displayExpenses() {
     }
 }
 
-// Event listener for form submission
-document.getElementById("submit-button").addEventListener("click", function (e) {
-    e.preventDefault();
-    console.log("buttonhasclickeditself")
-    /* // Rest of the code
-
-
-    // Get form input values
-    const date = document.getElementById("date").value;
-    const amount = document.getElementById("amount").value;
-    const description = document.getElementById("description").value;
-    const paidBy = document.getElementById("paid-by").value;
-
-    // Add the expense to the list
-    addExpense(date, amount, description, paidBy);
-
-    // Display updated expenses
+window.onload=function(){
+    document.getElementById("expense-form").addEventListener("submit", function (e) {
+        e.preventDefault(); // Prevent page reload
+        // Rest of the code
+    
+    
+        // Get form input values
+        const date = document.getElementById("date").value;
+        const amount = document.getElementById("amount").value;
+        const description = document.getElementById("description").value;
+        const paidBy = document.getElementById("paid-by").value;
+    
+        // Add the expense to the list
+        addExpense(date, amount, description, paidBy);
+    
+        // Display updated expenses
+        displayExpenses();
+    
+        // Clear form fields
+        document.getElementById("date").value = "";
+        document.getElementById("amount").value = "";
+        document.getElementById("description").value = "";
+        document.getElementById("paid-by").value = "";
+    });
     displayExpenses();
-
-    // Clear form fields
-    document.getElementById("date").value = "";
-    document.getElementById("amount").value = "";
-    document.getElementById("description").value = "";
-    document.getElementById("paid-by").value = ""; */
-});
-
+}
 // Initial display of expenses
-displayExpenses();
+
